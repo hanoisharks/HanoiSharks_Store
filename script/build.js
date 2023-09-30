25b8fe00c8020ce6cf806672806a6620930a47c6
Tienzwuang <tienquang090901@gmail.com>
2023-10-01 04:29:01 +0700

    Thêm tất cả các tệp mới và thay đổi

---
+++ script/build.js
@@ -0,0 +1,32 @@
+let productHTML = '' ;
+
+let search = '' ;
+
+product.forEach((product) =>  {
+
+  if(product.keyword.includes(search)) {
+    const priceInVND = (product.price * 1000).toLocaleString('vi-VN', { style: 'currency', currency: 'VND' });
+
+    productHTML += `
+      <div class="pro" onclick="window.location.href='sproduct${product.id}.html'">
+        <img src="img/shoes/${product.img}" alt="">
+        <div class="des">
+          <span>Hanoi Sharks</span>
+          <h5>${product.name}</h5>
+          <div class="star">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+          </div>
+          <h4>${priceInVND}</h4>
+        </div>
+        <a href="#"><img class="cart" src="cart_icon.png" alt=""></a>
+      </div>
+    `;
+  }
+});
+
+document.querySelector('.js-pro-container').
+innerHTML = productHTML;
