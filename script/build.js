ed336de805989fc28cdc3178bb7508b36a6221a2
Tienzwuang <tienquang090901@gmail.com>
2023-11-24 12:08:54 +0700

    add sale black friday

--- script/build.js
+++ script/build.js
@@ -6,11 +6,19 @@ let search = '' ;
 product.forEach((product) =>  {
 
   if(product.keyword.includes(search)) {
-    const priceInVND = (product.price * 1000).toLocaleString('vi-VN', { style: 'currency', currency: 'VND' });
-
+    const priceInVND = (product.old_price * 1000).toLocaleString('vi-VN', { style: 'currency', currency: 'VND' });
+    const salePrice = (product.price * 1000).toLocaleString('vi-VN', { style: 'currency', currency: 'VND' });
+    const salePercent = ((product.old_price - product.price) / product.price) * 100;
+    const formattedSalePercent = salePercent.toString().split('.')[0];
     productHTML += `
       <div class="pro" onclick="window.location.href='sproduct${product.id}.html'">
-        <img src="img/shoes/${product.img}" alt="">
+      <span class="discount">
+          <strong>
+            <i class="fa-solid fa-heart"></i>
+            Giảm ${formattedSalePercent}%
+          </strong>
+        </span>
+      <img src="img/shoes/${product.img}" alt="">
         <div class="des">
           <span>Hanoi Sharks</span>
           <h5>${product.name}</h5>
@@ -21,7 +29,8 @@ product.forEach((product) =>  {
             <i class="fas fa-star"></i>
             <i class="fas fa-star"></i>
           </div>
-          <h4>${priceInVND}</h4>
+          <h4 id="old-price">${priceInVND}</h4>
+          <h4 id="new-price">${salePrice}</h4>
         </div>
         <a href="#"><img class="cart" src="cart_icon.png" alt=""></a>
       </div>
--- script/buildNewProduct.js
+++ script/buildNewProduct.js
@@ -7,25 +7,34 @@ product.forEach((product) =>  {
 
   if(product.keyword.includes(search)) {
     priceInVND = (product.price * 1000).toLocaleString('vi-VN', { style: 'currency', currency: 'VND' });
-
+    salePrice = (product.price * 1000).toLocaleString('vi-VN', { style: 'currency', currency: 'VND' });
+    salePercent = ((product.old_price - product.price) / product.price) * 100;
+    formattedSalePercent = salePercent.toString().split('.')[0];
     NewProductHTML += `
-      <div class="pro" onclick="window.location.href='sproduct${product.id}.html'">
-        <img src="img/shoes/${product.img}" alt="">
-        <div class="des">
-          <span>Hanoi Sharks</span>
-          <h5>${product.name}</h5>
-          <div class="star">
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-          </div>
-          <h4>${priceInVND}</h4>
+    <div class="pro" onclick="window.location.href='sproduct${product.id}.html'">
+    <span class="discount">
+        <strong>
+          <i class="fa-solid fa-heart"></i>
+          Giảm ${formattedSalePercent}%
+        </strong>
+      </span>
+    <img src="img/shoes/${product.img}" alt="">
+      <div class="des">
+        <span>Hanoi Sharks</span>
+        <h5>${product.name}</h5>
+        <div class="star">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
         </div>
-        <a href="#"><img class="cart" src="cart_icon.png" alt=""></a>
+        <h4 id="old-price">${priceInVND}</h4>
+        <h4 id="new-price">${salePrice}</h4>
       </div>
-    `;
+      <a href="#"><img class="cart" src="cart_icon.png" alt=""></a>
+    </div>
+  `;
   }
 });
 
